988f18845d8f8c05d6691f36965b77a2e7b57f92
Ben <bgoober2@protonmail.com>
2023-08-15 23:24:22 -0400

    refactors

--- background.js
+++ background.js
@@ -1,20 +1,58 @@
 chrome.action.onClicked.addListener((tab) => {
+    // Step 1: Inject Readability library
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ['libs/readability.js']
     }, () => {
+        // Step 2: Parse content with Readability
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             code: `
                 const documentClone = document.cloneNode(true);
                 const article = new Readability(documentClone).parse();
-                article.content;
+                article ? article.content : '';
             `
         }, ([result]) => {
-            const readerWin = window.open('reader.html', 'reader_view', 'width=800,height=600');
-            readerWin.onload = function () {
-                readerWin.document.body.innerHTML = result.content;
-            };
+            if (result && result.content) {
+                // Step 3: Replace the current page content with parsed content
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    code: `
+                        document.open();
+                        document.write(\`${result.content}\`);
+                        document.close();
+                    `
+                }, () => {
+                    // Step 4: Apply the reader.css styling
+                    chrome.scripting.insertCSS({
+                        target: { tabId: tab.id },
+                        files: ['styles/reader.css']
+                    });
+                });
+            }
         });
     });
 });
+
+
+
+// chrome.action.onClicked.addListener((tab) => {
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         files: ['libs/readability.js']
+//     }, () => {
+//         chrome.scripting.executeScript({
+//             target: { tabId: tab.id },
+//             code: `
+//                 const documentClone = document.cloneNode(true);
+//                 const article = new Readability(documentClone).parse();
+//                 article.content;
+//             `
+//         }, ([result]) => {
+//             const readerWin = window.open('reader.html', 'reader_view', 'width=800,height=600');
+//             readerWin.onload = function () {
+//                 readerWin.document.body.innerHTML = result.content;
+//             };
+//         });
+//     });
+// });
