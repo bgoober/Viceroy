18b2b64c707af1dd87985094a1be1990eaad0c14
Ben <bgoober2@protonmail.com>
2023-08-15 20:51:30 -0400

    edit multiple files

--- content.js
+++ content.js
@@ -1,32 +1,57 @@
-function parseAndDisplayContent() {
-  // console.log for debugging
-  console.log("Parsing and displaying content...");
+const article = document.querySelector("article");
 
-  const article = new Readability(document).parse();
-  const contentDiv = document.createElement("div");
-  contentDiv.innerHTML = article.content;
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
 
-  // Style images or other media within the content
-  const images = contentDiv.querySelectorAll("img");
-  images.forEach((img) => {
-    img.style.maxWidth = "100%";
-    img.style.height = "auto";
-    img.style.margin = "10px 0";
-  });
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
 
-  document.body.innerHTML = "";
-  document.body.appendChild(contentDiv);
+  (date ?? heading).insertAdjacentElement("afterend", badge);
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // console.log for debugging
-  console.log("Content script received a message:", request);
 
-  if (request.action === "startParsing") {
-    // console.log for debugging
-    console.log("Received 'startParsing' action.");
 
-    parseAndDisplayContent();
-    sendResponse({ status: "success" });
-  }
-});
+// function parseAndDisplayContent() {
+//   // console.log for debugging
+//   console.log("Parsing and displaying content...");
+
+//   const article = new Readability(document).parse();
+//   const contentDiv = document.createElement("div");
+//   contentDiv.innerHTML = article.content;
+
+//   // Style images or other media within the content
+//   const images = contentDiv.querySelectorAll("img");
+//   images.forEach((img) => {
+//     img.style.maxWidth = "100%";
+//     img.style.height = "auto";
+//     img.style.margin = "10px 0";
+//   });
+
+//   document.body.innerHTML = "";
+//   document.body.appendChild(contentDiv);
+// }
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   // console.log for debugging
+//   console.log("Content script received a message:", request);
+
+//   if (request.action === "startParsing") {
+//     // console.log for debugging
+//     console.log("Received 'startParsing' action.");
+
+//     parseAndDisplayContent();
+//     sendResponse({ status: "success" });
+//   }
+// });
