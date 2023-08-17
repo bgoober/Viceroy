27e29387b9d6ff053b0346e2d726eff135203b64
Ben <bgoober2@protonmail.com>
2023-08-17 03:34:22 -0400

    edit background and readme

--- background.js
+++ background.js
@@ -91,10 +91,10 @@ chrome.tabs.onRemoved.addListener((tabId) => {
     }
 });
 
-chrome.commands.onCommand.addListener(function(command) {
+chrome.commands.onCommand.addListener(function (command) {
     console.log("Command received:", command);
-    if (command === "_execute_browser_action" || command === "_execute_action") {  
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    if (command === "_execute_browser_action" || command === "_execute_action") {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             const currentTab = tabs[0];
             if (currentTab) {
                 toggleReaderView(currentTab);
@@ -102,69 +102,3 @@ chrome.commands.onCommand.addListener(function(command) {
         });
     }
 });
-
-
-
-// chrome.action.onClicked.addListener((tab) => {
-//     // Step 1: Inject Readability library
-//     chrome.scripting.executeScript({
-//         target: { tabId: tab.id },
-//         files: ['libs/readability.js']
-//     }, (injectionResults) => {
-//         if (chrome.runtime.lastError) {
-//             console.error('Error injecting Readability:', chrome.runtime.lastError);
-//             return;
-//         }
-
-//         console.log('Readability library injected:', injectionResults);
-
-//         // Step 2: Parse content with Readability
-//         chrome.scripting.executeScript({
-//             target: { tabId: tab.id },
-//             func: function () {
-//                 const documentClone = document.cloneNode(true);
-//                 const article = new Readability(documentClone).parse();
-//                 return article ? article.content : '';
-//             }
-//         }, ([result]) => {
-//             console.log('Content parsed with Readability:', result);
-
-//             // Detailed structure log
-//             console.log('Detailed structure of the result:', JSON.stringify(result, null, 2));
-
-//             // Check if there's content and handle accordingly
-//             const articleContent = result.result || (result[0] && result[0].result);
-//             if (articleContent) {
-//                 // Step 3: Replace the current page content with parsed content
-//                 chrome.scripting.executeScript({
-//                     target: { tabId: tab.id },
-//                     func: function (content) {
-//                         document.open();
-//                         document.write(content);
-//                         document.close();
-//                     },
-//                     args: [articleContent]
-//                 }, (insertionResults) => {
-//                     if (chrome.runtime.lastError) {
-//                         console.error('Error replacing content:', chrome.runtime.lastError);
-//                         return;
-//                     }
-
-//                     // Step 4: Apply the reader.css styling
-//                     chrome.scripting.insertCSS({
-//                         target: { tabId: tab.id },
-//                         files: ['reader.css']
-//                     }, (styleResults) => {
-//                         if (chrome.runtime.lastError) {
-//                             console.error('Error applying CSS:', chrome.runtime.lastError);
-//                         }
-
-//                         console.log('CSS applied:', styleResults);
-//                     });
-//                 });
-//             } else {
-//                 console.warn('No parsed content received.');
-//             }
-//         });
-//     });
-// });
