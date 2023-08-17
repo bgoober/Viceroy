34f4e0959779681aa4680e171fd9379f9310f4b2
Ben <bgoober2@protonmail.com>
2023-08-17 01:58:59 -0400

    modify scripts

--- background.js
+++ background.js
@@ -1,73 +1,63 @@
 chrome.action.onClicked.addListener((tab) => {
-    console.log('Extension icon clicked for tab:', tab.id); // Debug line
-
     // Step 1: Inject Readability library
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ['libs/readability.js']
-    }, () => {
-        console.log('Readability library injected'); // Debug line
-        
+    }, (injectionResults) => {
+        if (chrome.runtime.lastError) {
+            console.error('Error injecting Readability:', chrome.runtime.lastError);
+            return;
+        }
+
+        console.log('Readability library injected:', injectionResults);
+
         // Step 2: Parse content with Readability
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: function() {
+            func: function () {
                 const documentClone = document.cloneNode(true);
                 const article = new Readability(documentClone).parse();
-                console.log('Article parsed:', article); // Debug line inside content script
                 return article ? article.content : '';
             }
         }, ([result]) => {
-            if (result && result.content) {
-                console.log('Parsed content received, about to replace page content'); // Debug line
+            console.log('Content parsed with Readability:', result);
 
+            // Detailed structure log
+            console.log('Detailed structure of the result:', JSON.stringify(result, null, 2));
+
+            // Check if there's content and handle accordingly
+            const articleContent = result.result || (result[0] && result[0].result);
+            if (articleContent) {
                 // Step 3: Replace the current page content with parsed content
                 chrome.scripting.executeScript({
                     target: { tabId: tab.id },
-                    func: function(content) {
+                    func: function (content) {
                         document.open();
                         document.write(content);
                         document.close();
                     },
-                    args: [result.content]
-                }, () => {
-                    console.log('Page content replaced. Injecting CSS now'); // Debug line
-                    
+                    args: [articleContent]
+                }, (insertionResults) => {
+                    if (chrome.runtime.lastError) {
+                        console.error('Error replacing content:', chrome.runtime.lastError);
+                        return;
+                    }
+
                     // Step 4: Apply the reader.css styling
                     chrome.scripting.insertCSS({
                         target: { tabId: tab.id },
                         files: ['reader.css']
-                    }, () => {
-                        console.log('CSS injected successfully'); // Debug line
+                    }, (styleResults) => {
+                        if (chrome.runtime.lastError) {
+                            console.error('Error applying CSS:', chrome.runtime.lastError);
+                        }
+
+                        console.log('CSS applied:', styleResults);
                     });
                 });
             } else {
-                console.log('No parsed content received'); // Debug line
+                console.warn('No parsed content received.');
             }
         });
     });
 });
-
-
-
-
-// chrome.action.onClicked.addListener((tab) => {
-//     chrome.scripting.executeScript({
-//         target: { tabId: tab.id },
-//         files: ['libs/readability.js']
-//     }, () => {
-//         chrome.scripting.executeScript({
-//             target: { tabId: tab.id },
-//             code: `
-//                 const documentClone = document.cloneNode(true);
-//                 const article = new Readability(documentClone).parse();
-//                 article.content;
-//             `
-//         }, ([result]) => {
-//             const readerWin = window.open('reader.html', 'reader_view', 'width=800,height=600');
-//             readerWin.onload = function () {
-//                 readerWin.document.body.innerHTML = result.content;
-//             };
-//         });
-//     });
-// });
